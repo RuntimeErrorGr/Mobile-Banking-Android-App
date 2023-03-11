d3d9e5fe188498fa6d118b84df96dc411114190e
Lăbău-Cristea Andrei-Liviu <andrei_liviu.labau@stud.acs.upb.ro>
2023-03-11 22:05:40 +0200

    Add project skel

---
+++ app/src/main/java/eim/project/mobile_banking_android_app/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package eim.project.mobile_banking_android_app.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
